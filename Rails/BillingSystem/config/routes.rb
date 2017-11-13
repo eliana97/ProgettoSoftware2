3af83909508d7efe879e8ccb66c1620f991fd7e8
alberto97tn <dietre.alberto@gmail.com>
2017-11-13 18:00:56 +0100

    Correct homepage problem

--- Rails/BillingSystem/config/routes.rb
+++ Rails/BillingSystem/config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :activities
   resources :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to:"welcome#show"
+  root to: 'welcome#show'
 
 end
